3ea8b25f76d9740fb11a02640341f2d3ff155f2d
FAHIM877827 <mohamedfahimupm786@gmail.com>
2026-07-05 22:10:36 +0530

    chore: initialize Interview OS project

---
+++ backend/src/test/java/com/interviewos/backends/BackendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewos.backends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
